916a688649681ed273a842755dba7824d722f284
WittyDelonix <vuviet5703@gmail.com>
2026-01-30 02:45:00 +0700

    Update create-api-token.sh

--- scripts/create-api-token.sh
+++ scripts/create-api-token.sh
@@ -13,7 +13,7 @@ echo ""
 pveum user add terraform@pam --comment "Terraform automation user"
 
 # Create role with necessary permissions
-pveum role add TerraformRole -privs "Datastore.AllocateSpace Datastore.AllocateTemplate Datastore.Audit Pool.Allocate Pool.Audit Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.PowerMgmt SDN.Use"
+pveum role add TerraformRole -privs "Datastore.AllocateSpace Datastore.AllocateTemplate Datastore.Audit Pool.Allocate Pool.Audit Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.PowerMgmt SDN.Use VM.GuestAgent.Audit VM.GuestAgent.Unrestricted"
 
 # Add role to user
 pveum aclmod / -user terraform@pam -role TerraformRole
