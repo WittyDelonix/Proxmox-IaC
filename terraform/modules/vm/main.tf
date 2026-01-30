f7f6439ae4facb2b2213f791bbe9cbcbf9228bc6
WittyDelonix <vuviet5703@gmail.com>
2026-01-30 11:16:38 +0700

    ghn

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "~> 3.0.1-rc1"
+      version = "3.0.2-rc07"
     }
   }
 }
@@ -83,66 +83,66 @@ variable "cipassword" {
   default     = "password"
 }
 resource "proxmox_vm_qemu" "vm" {
-    vmid = var.vm_id
-    name = var.vm_name
-    target_node = var.proxmox_node
+  vmid        = var.vm_id
+  name        = var.vm_name
+  target_node = var.proxmox_node
 
-    clone = var.template_name
-    full_clone = false
-    bios = "ovmf"
-    agent = 1
-    scsihw = "virtio-scsi-single"
+  clone      = var.template_name
+  full_clone = false
+  bios       = "ovmf"
+  agent      = 1
+  scsihw     = "virtio-scsi-single"
 
-    os_type = "cloud-init"
-    memory = var.memory
+  os_type = "cloud-init"
+  memory  = var.memory
 
-    vm_state = "running"
-    onboot = true
-    startup = "order=1"
+  vm_state = "running"
+  onboot   = true
+  startup  = "order=1"
 
-    ipconfig0 = "p=${var.ip_address},gw=${var.gateway}"
-    skip_ipv6 = true
-    ciuser = "ubuntu"
-    cipassword = var.cipassword
-    sshkeys = var.ssh_public_key
-    tags = var.tags
+  ipconfig0  = "p=${var.ip_address},gw=${var.gateway}"
+  skip_ipv6  = true
+  ciuser     = "ubuntu"
+  cipassword = var.cipassword
+  sshkeys    = var.ssh_public_key
+  tags       = var.tags
 
-    cpu {
-        type = "x86-64-v2-AES"
-        sockets = 1
-        cores = var.cores
-    }
+  cpu {
+    type    = "x86-64-v2-AES"
+    sockets = 1
+    cores   = var.cores
+  }
 
-    serial {
-        id = 0
-        type = "socket"
-    }
+  serial {
+    id   = 0
+    type = "socket"
+  }
 
-    network {
-        id = 0
-        model = var.network_model
-        bridge = var.network_bridge
-        firewall = false
-    }
+  network {
+    id       = 0
+    model    = var.network_model
+    bridge   = var.network_bridge
+    firewall = false
+  }
 
-    disks {
-      scsi {
-          scsi0 {
-              disk {
-                  size = var.disk_size
-                  storage = var.storage
-                  replicate = "true"
-              }
-          }
-      }
-      ide {
-          ide0 {
-              cloudinit {
-                  storage = var.storage
-              }
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          size      = var.disk_size
+          storage   = var.storage
+          replicate = "true"
         }
       }
     }
+    ide {
+      ide0 {
+        cloudinit {
+          storage = var.storage
+        }
+      }
+    }
+  }
 }
 
 output "vm_id" {
